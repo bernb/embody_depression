52ccf20422b7ff5f0cb0f5c70dcf0edb14b37dd4
bernb <15048930+bernb@users.noreply.github.com>
2019-07-06 13:34:34 +0200

    Build multiple_comparison_correction to use FDR method

--- helpers/multiple_comparison_correction.m
+++ helpers/multiple_comparison_correction.m
@@ -1,4 +1,19 @@
-function [outputArg1,outputArg2] = multiple_comparison_correction(inputArg1,inputArg2)
+function [t_thres, t_thres_no_assump] = multiple_comparison_correction(data, subject_count)
+% Input: Data for FDR testing
+% Output: 
+% t_thres: t threshold for assumption of independence or positive
+% correlation
+% t_thres_no_assump: t threshold for no assumption about correlation
+
+% ToDo: Is this really needed? We do not work with 'demo subjects'
+data(~isfinite(alltdata)) = [];   % getting rid of anomalies due to low number of demo subjects (ie no variance)
+
+df=subject_count-1;    % degrees of freedom
+P = 1-cdf('T',data,df);  % p values
+[pID, pN] = FDR(P,0.05);             % BH FDR
+t_thres = icdf('T',1-pID,df);      % T threshold, indep or pos. correl.
+t_thres_no_assump = icdf('T',1-pN,df) ;      % T threshold, no correl. assumptions
+
 
 end
 

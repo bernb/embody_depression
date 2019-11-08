e9875e3c91067534e41fc062acdb755a46bdc35f
Bernard Beitz <15048930+bernb@users.noreply.github.com>
2019-11-08 14:45:54 +0100

    Show correct colorbar

--- +helpers/+plot/data.m
+++ +helpers/+plot/data.m
@@ -12,16 +12,17 @@ data_max = max(data, [], 'all');
 color_limits = [-data_max,data_max];
 hotmap = hot(num_colors);
 coldmap = flipud([hotmap(:,3) hotmap(:,2) hotmap(:,1) ]);
-hotcoldmap = [coldmap; hotmap];
+color_map = [coldmap; hotmap];
 
 for n = 1:plot_count
    % We leave one empty to attach colorbar after the loop
    subplot(1, plot_count+1, n); 
-   helpers.plot.single_figure(data(:,:,n), hotcoldmap, color_limits);
+   helpers.plot.single_figure(data(:,:,n), color_map, color_limits);
 end
-image_handle = imagesc(ones(size(data(:,:,1))), color_limits);
+subplot(1, plot_count+1, plot_count+1);
+image_handle = helpers.plot.single_figure(data(:,:,n), color_map, color_limits);
 axis_handle = image_handle.Parent;
-colorbar(axis_handle);
+c_bar = colorbar(axis_handle);
 image_handle.Visible = false;
 axis_handle.Visible = false;
 
--- workflow.m
+++ workflow.m
@@ -1,4 +1,3 @@
-basepath = 'data/subjects';
 [activation_data, averaged_data] = preprocess_data(basepath);
 t_data = calc_embody_t_values(averaged_data);
 %helpers.plot.data(t_data);

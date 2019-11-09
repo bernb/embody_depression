b8bb0f91716b08ce2461a83220859ba3bcf6c454
Bernard Beitz <15048930+bernb@users.noreply.github.com>
2019-11-09 12:09:34 +0100

    Use default parameter for single figure for easier on the fly plotting

--- +helpers/+plot/single_figure.m
+++ +helpers/+plot/single_figure.m
@@ -1,5 +1,13 @@
 function image_handle = single_figure(data, color_map, color_limits)
 
+if nargin < 3
+    data_max = max(1, max(data, [], 'all'));
+    color_limits = [-data_max, data_max];
+end
+if nargin < 2
+    color_map = hot(64);
+end
+
 image_handle = imagesc(data, color_limits);
 axes = image_handle.Parent;
 mask = imread('images/mask.png');

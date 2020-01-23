5a2b45325c2152bd8dc1e043af0f9db22420b33c
Bernard Beitz <15048930+bernb@users.noreply.github.com>
2020-01-23 09:22:22 +0100

    Adjust confusion_matrix plot properties

--- +helpers/+plot/confusion_matrix.m
+++ +helpers/+plot/confusion_matrix.m
@@ -7,11 +7,10 @@ function image_handle = confusion_matrix(confusion_table)
 % 750/6 = 120
 lim_max = sum(confusion_table{1,:}, 2);
 limits = [0 lim_max];
-
 chance_level = round(lim_max / height(confusion_table));
+prop_count = length(confusion_table.Properties.VariableNames);
 
 % We color in white everything below chance level (lim_max / emotion_count)
-% ToDo: Don't hardcode white limit
 graymap = gray(lim_max - chance_level);
 graymap = flipud(graymap);
 map = [ones(chance_level,3); graymap];
@@ -21,5 +20,21 @@ image_handle = imagesc(confusion_table.Variables, limits);
 axes = image_handle.Parent;
 colorbar(axes);
 colormap(axes, map);
+
+% Always keep 1:1 aspect ratio
+daspect([1 1 1 ]);
+
+% Tick Properties
+yticks(1:prop_count);
+xticks(1:prop_count);
+if prop_count == 6
+    xticklabels({'Anger', 'Disgust', 'Happiness', 'Sadness', 'Fear', 'Neutral'});
+    yticklabels({'Anger', 'Disgust', 'Happiness', 'Sadness', 'Fear', 'Neutral'});
+else
+    xticklabels({'Disgust', 'Happiness', 'Sadness', 'Fear', 'Neutral'});
+    yticklabels({'Disgust', 'Happiness', 'Sadness', 'Fear', 'Neutral'});
+end
+xtickangle(45);
+axes.TickLength = [0 0];
 end
 
